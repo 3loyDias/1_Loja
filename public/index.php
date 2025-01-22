e0eee32948a2d21dfa53f3759c4ee752ee6adf2c
Izbunia <24669@aeffl.pt>
2025-01-22 08:49:17 +0000

    BD Connect

--- public/index.php
+++ public/index.php
@@ -9,10 +9,4 @@ session_start();
 require_once '../config.php';
 require_once '../vendor/autoload.php';
 
-$bd = new database();
-$clientes = $bd->select("SELECT  nome FROM clientes");
-print_r($clientes);
-echo "<pre>";
-echo $clientes[0]->nome. "<br>";
-echo $clientes[2]->nome. "<br>";
 

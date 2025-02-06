5777794da311d0d5effa8513cfc9da18275e118f
Izbunia <24669@aeffl.pt>
2025-02-06 10:07:26 +0000

    #

--- core/views/criar_cliente_sucesso.php
+++ core/views/criar_cliente_sucesso.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row my-5">
         <div class="col-sm-6 offset-sm-3">
-            <h3 class="text-center">Confirmcação do email!</h3>
+            <h3 class="text-center">Confirmação do email!</h3>
             <p> Foi enviado um email com o link, para confirmação da sua
                 Conta</p>
             <p> Verifique se o email aparece na sua Conta ou se foi para a
